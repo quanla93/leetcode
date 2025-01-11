95cb999e8b01d466ce169a5f9b760d5626a70520
QuanLA <quanla.work@gmail.com>
2025-01-11 22:26:04 +0700

    [DAILY] 1400. Construct K Palindrome Strings Refs leetcode from quanla (11/1/2025)

---
+++ src/daily/day_11_1_2025.java
@@ -0,0 +1,31 @@
+package daily;
+
+public class day_11_1_2025 {
+
+    public static void main(String[] args) {
+        day_11_1_2025 obj = new day_11_1_2025();
+        System.out.println(obj.canConstruct("annabelle", 2));
+        System.out.println(obj.canConstruct("leetcode", 3));
+        System.out.println(obj.canConstruct("true", 4));
+        System.out.println(obj.canConstruct("cr", 4));
+        System.out.println(obj.canConstruct("putmeinatrouble", 16));
+    }
+
+    public boolean canConstruct(String s, int k) {
+        if (s.length() < k) {
+            return false;
+        }
+        int[] countChar = new int[26];
+        for (char ch: s.toCharArray()) {
+            countChar[ch - 'a']++;
+        }
+
+        int odd_count = 0;
+        for (int i = 0; i < countChar.length; i++ ) {
+            if (countChar[i] % 2 != 0) {
+                odd_count++;
+            }
+        }
+        return odd_count <= k;
+    }
+}
