c54f8f6e80fd1460351604ddf476c4c619f98be7
QuanLA <quanla.work@gmail.com>
2025-01-16 23:06:10 +0700

    [DAILY] 2425. Bitwise XOR of All Pairings Refs leetcode from quanla (16/1/2025)

---
+++ src/daily/day_16_1_2025.java
@@ -0,0 +1,42 @@
+package daily;
+
+public class day_16_1_2025 {
+
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        int xorNums1 = 0;
+        int xorNums2 = 0;
+
+        // Tính XOR của tất cả phần tử trong nums1
+        for (int num : nums1) {
+            xorNums1 ^= num;
+        }
+
+        // Tính XOR của tất cả phần tử trong nums2
+        for (int num : nums2) {
+            xorNums2 ^= num;
+        }
+
+        int result = 0;
+        if (nums1.length % 2 == 1) {
+            result ^= xorNums2;
+        }
+        if (nums2.length % 2 == 1) {
+            result ^= xorNums1;
+        }
+
+        // Kết quả cuối cùng
+        return result;
+    }
+
+    public static void main(String[] args) {
+        day_16_1_2025 obj = new day_16_1_2025();
+
+        int[] nums1 = {1, 2};
+        int[] nums2 = {3, 4};
+        System.out.println(obj.xorAllNums(nums1, nums2));  // Output: 2
+
+        int[] nums1_2 = {2, 1, 3};
+        int[] nums2_2 = {10, 2, 5, 0};
+        System.out.println(obj.xorAllNums(nums1_2, nums2_2));  // Output: 13
+    }
+}
